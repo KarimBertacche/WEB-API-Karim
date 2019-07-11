5f84154796d28cbee5d4863bd066982d453a0b5b
Karim B <mattiakarim@live.it>
2019-07-11 15:38:07 +0200

    Chore: added first get request for users

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const url = 'https://webapi-karim-user-posts.herokuapp.com/';
+// const url = 'https://localhost:4000/';
+
+class App extends React.Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    this.getRequestUsersHandler();
+  }
+
+  getRequestUsersHandler = () => {
+    axios 
+      .get(`${url}/api/users`)
+        .then(response => {
+          console.log(response);
+          debugger
+        })
+        .catch(error => {
+          debugger
+        });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Working react app
+      </div>
+    );
+  }
 }
 
 export default App;
