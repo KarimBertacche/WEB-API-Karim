67c7171d36101fbb469685e887fdf33953853827
Karim B <mattiakarim@live.it>
2019-07-11 14:40:10 +0200

    Features: made some changes to the server and index files

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors');
-const postRoute = require('./posts/postRouter');
+const server = require('./server');
 
 const port = process.env.PORT;
-
-const server = express();
-
-server.use(cors());
-server.use(postRoute);
 
 server.listen(port, () => {
     console.log(`** Server running on port ${port} **`);
